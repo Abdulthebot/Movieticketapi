1fa98e4fea34a473890df41497fe2e1e4d047ab1
abdul <hameedd965@gmail.com>
2025-10-06 02:25:16 +0530

    FIX: Add project root to Python path

--- movie_booking/settings.py
+++ movie_booking/settings.py
@@ -1,9 +1,10 @@
 import os
 import dj_database_url 
+import sys
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
-import sys 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
